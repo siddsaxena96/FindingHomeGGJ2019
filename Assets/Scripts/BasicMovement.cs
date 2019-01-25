87da4fce4af4b138457a64360b57c84c4ce3ff15
Skiggs <sidbhati32@gmail.com>
2019-01-26 04:18:08 +0530

    movement mechanics

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -5,26 +5,52 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float thrust=2f;
+    public float thrust;
+    public float fwdSpeed;
+    public Vector3 jumpForce;
     public Animator walkingAnim;
-    
+    public GameObject floor;
     void Start()
     {
         rb = GetComponent<Rigidbody2D> ();
+        jumpForce = new Vector3(0.0f, thrust, 0.0f);
     }
     
     void Update()
     {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
-        Vector3 movement = new Vector3 (moveHorizontal, moveVertical,0 );
-        rb.velocity = movement * thrust;
+        bool groundInst = floor.GetComponent<FloorBehave>().isGrounded;
         
+        
+
+        if(Input.GetKeyDown(KeyCode.Space) && groundInst == true )
+        {
+
+            rb.gravityScale = 1.0f;
+            rb.AddForce(jumpForce);
+
+        }
+
+        if(groundInst == false)
+        {
+            rb.gravityScale = 10.0f;
+        }
+
+          else if(groundInst == true)
+        {
+            rb.gravityScale = 1.0f;
+
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+            rb.velocity = movement * fwdSpeed;
+        }
+
+
         if(rb.velocity==Vector2.zero){                 
             walkingAnim.Play("NONE");
         }
-        else{
+       /* else{
             walkingAnim.Play("Walking");
-        }
+        }*/
     }
 }
