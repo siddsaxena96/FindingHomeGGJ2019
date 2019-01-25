b57b1c7ab99e1e2fe15da5f6e607600905b642f7
Siddharth Saxena <sidd.saxena96@gmail.com>
2019-01-26 01:43:50 +0530

    msg1

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float thrust=2f;
+    public Animator walkingAnim;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+    
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector3 movement = new Vector3 (moveHorizontal, moveVertical,0 );
+        rb.velocity = movement * thrust;
+        
+        if(rb.velocity==Vector2.zero){                 
+            walkingAnim.Play("NONE");
+        }
+        else{
+            walkingAnim.Play("Walking");
+        }
+    }
+}
