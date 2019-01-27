5f0e5bbc1425847fd858547ba40b566aa47ed31e
Siddharth Saxena <sidd.saxena96@gmail.com>
2019-01-27 11:18:03 +0530

    Fixing Animations

---
+++ Assets/Scripts/Bar/BarController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BarController : MonoBehaviour
+{
+    public AudioSource barConversation;
+    public BasicMovement playerControl;
+    public GameObject messageBox;
+    private bool spaceDisable=true;
+
+    void Start(){
+        StartCoroutine("checkPlay");
+    }
+
+    IEnumerator checkPlay(){
+        while(barConversation.isPlaying){
+            yield return new WaitForSeconds(0.15f);
+        }
+        playerControl.move=true;
+        messageBox.SetActive(true);
+        spaceDisable=false;
+    }
+
+    void Update(){
+        if(!spaceDisable){
+            if(Input.GetKey(KeyCode.Space)){
+            //    SceneManager.LoadScene("BarScene");  
+            }
+        }
+    }
+
+}
